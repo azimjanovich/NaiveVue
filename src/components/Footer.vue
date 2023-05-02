eeebbe707b2627e2bd24af82d8110f57268c3353
Javlonbek Tuychiyev <president7566@gmail.com>
2023-05-02 15:18:26 +0500

    fix and add vue-cookies, vue-router

---
+++ src/components/Footer.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        This is footer
+    </div>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import VueCookies from 'vue-cookies'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(createPinia())
+app.use(VueCookies, { expire: "7d" })
+app.mount('#app')
