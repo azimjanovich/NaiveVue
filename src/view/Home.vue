97da52cdacc71216d40e11ce351b4a33e2b7383e
Javlonbek Tuychiyev <president7566@gmail.com>
2023-06-04 17:01:49 +0500

    fix .env

--- src/view/Home.vue
+++ src/view/Home.vue
@@ -1,3 +1,6 @@
+<script setup>
+console.log(import.meta.env.VITE_API_ENDPOINT);
+</script>
 <template>
     <div class="bg-red-600 min-h-[400px] rounded">
     </div>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-})
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  return defineConfig({
+    plugins: [vue()]
+  })
+}
